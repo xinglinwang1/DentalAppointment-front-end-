b40e5576e31878244812c8834b2aa7becdb2629f
doc.x.archer <104256701+archer0x@users.noreply.github.com>
2024-01-03 23:09:09 +0800

    Update AdminHomeView.vue

--- src/views/AdminHomeView.vue
+++ src/views/AdminHomeView.vue
@@ -23,7 +23,8 @@
 
           <div class="table_doctor">
             <!-- 表格组件：医生信息 -->
-            <el-table v-if="activeTab === 'doctor-info'" :data="paginatedReserveInfoList_Doctor" stripe style="width: 100%">
+            <el-table v-if="activeTab === 'doctor-info'" :data="paginatedReserveInfoList_Doctor" stripe
+              style="width: 100%">
               <!-- 表格列定义，根据实际数据结构进行调整 -->
               <el-table-column label="ID" prop="id"></el-table-column>
               <el-table-column label="医生用户名" prop="doctorUsername"></el-table-column>
@@ -39,13 +40,17 @@
                   <el-button type="text" @click="Reject(scope.row, '医生')" style="color: rgb(255, 2, 2);">×</el-button>
                 </template>
               </el-table-column>
+              <el-table-column label="操作" align="center">
+                <template v-slot="scope">
+                  <el-button plain type="danger" size="mini" @click="seeKpi(scope.row.doctorUsername)">查看kpi</el-button>
+                </template>
+              </el-table-column>
             </el-table>
             <!-- 分页组件 -->
             <div class="pagination_d">
               <div class="doctor">
                 <el-pagination :hide-on-single-page="true" :current-page="currentPage_doctor" :page-size="pageSize_doctor"
-                 layout="prev, pager, next, jumper"
-                  :total="Doctor_display.length"
+                  layout="prev, pager, next, jumper" :total="Doctor_display.length"
                   @current-change="handleCurrentChange_doctor" />
               </div>
             </div>
@@ -54,7 +59,8 @@
 
           <div class="table_patient">
             <!-- 表格组件：新接口返回的数据 -->
-            <el-table v-if="activeTab === 'patient-info'" :data="paginatedReserveInfoList_Patient" stripe style="width: 100%">
+            <el-table v-if="activeTab === 'patient-info'" :data="paginatedReserveInfoList_Patient" stripe
+              style="width: 100%">
               <!-- 表格列定义，与第一个表格一致 -->
               <el-table-column label="ID" prop="id"></el-table-column>
               <el-table-column label="患者用户名" prop="patientUsername"></el-table-column>
@@ -73,13 +79,9 @@
             <!-- 分页组件 -->
             <div class="pagination_p">
               <div class="patient">
-                <el-pagination 
-                :hide-on-single-page="true"
-                :current-page="currentPage_patient" 
-                :page-size="pageSize_patient"
-                layout="prev, pager, next, jumper"
-                :total="Patient_display.length" 
-                @current-change="handleCurrentChange_patient" />
+                <el-pagination :hide-on-single-page="true" :current-page="currentPage_patient"
+                  :page-size="pageSize_patient" layout="prev, pager, next, jumper" :total="Patient_display.length"
+                  @current-change="handleCurrentChange_patient" />
               </div>
             </div>
           </div>
@@ -87,6 +89,14 @@
         </el-tabs>
       </el-main>
     </el-container>
+    <!-- kpi -->
+    <el-dialog v-model="kpiVisible" width="60%" title="kpi" align-center>
+      <div v-for="kpiInfo in kpiInfos" :key="kpiInfo.date">
+        <div>日期：{{ kpiInfo.date }}</div>
+        <div>kpi：{{ kpiInfo.kpi }}</div>
+        <br />
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -112,11 +122,17 @@ export default defineComponent({
       currentPage_doctor: 1,
       pageSize_patient: 10,
       pageSize_doctor: 10,
+      kpiVisible: false,
+      kpiInfos: [],
+      doctorInfo: {},
+      currentDate: new Date(),
+      formattedDate: '',
     };
   },
   mounted() {
     this.getDoctorData();
     this.getPatientData();
+    this.formatDate();
   },
   computed: {
     // 分页预约数据
@@ -132,15 +148,21 @@ export default defineComponent({
     },
   },
   methods: {
+    formatDate() {
+      const year = this.currentDate.getFullYear();
+      const month = this.currentDate.getMonth() + 1; // Months are zero-based
+      this.formattedDate = `${year}-${month}`;
+      console.log(this.formattedDate);
+    },
     async getDoctorData() {
       try {
         const response = await axios.get('http://118.195.236.254:8301/api/approval/doctor', {
           params: {
-            adminUsername: store.state.username,
+            adminUsername: "ly",
           },
         });
         this.医生 = response.data.data;
-        this.Doctor_display=this.医生;
+        this.Doctor_display = this.医生;
       } catch (error) {
         console.error('Error fetching patient data:', error);
       }
@@ -149,14 +171,44 @@ export default defineComponent({
       try {
         const response = await axios.get('http://118.195.236.254:8301/api/approval/patient', {
           params: {
-            adminUsername: store.state.username,
+            adminUsername: "ly",
           },
         });
         this.患者 = response.data.data;
-        this.Patient_display=this.患者;
+        this.Patient_display = this.患者;
       } catch (error) {
         console.error('Error fetching new API data:', error);
       }
+    },
+    async getallinfo(doctorUsername) {
+      return new Promise((resolve, reject) => {
+        axios.get('http://121.43.108.102:8101/api/doctor/' + doctorUsername)
+          .then((response) => {
+            this.doctorInfo = response.data.data;
+            console.log(this.doctorInfo);
+            resolve();
+          })
+          .catch((error) => {
+            console.error('Error fetching patient data:', error);
+            reject(error);
+          });
+      });
+    },
+    async getKpi() {
+      //todo:日期格式化
+      try {
+        const response = await axios.get('http://118.195.236.254:8401/api/hospital/' + parseInt(this.doctorInfo.hospitalId) + '/kpi/doctor/' + this.doctorInfo.jobNumber + '/month/'+this.formattedDate);
+        this.kpiInfos = response.data.data;
+        console.log(this.kpiInfos);
+      } catch (error) {
+        console.error('Error fetching patient data:', error);
+      }
+    },
+    seeKpi(doctorUsername) {
+      this.getallinfo(doctorUsername).then(() => {
+        this.getKpi();
+      })
+      this.kpiVisible = true;
     },
     handleCurrentChange_patient(newPage) {
       this.currentPage_patient = newPage;
@@ -210,8 +262,8 @@ export default defineComponent({
       }
     },
     handleSelect(key) {
-      this.currentPage_patient=1;
-      this.currentPage_doctor=1;
+      this.currentPage_patient = 1;
+      this.currentPage_doctor = 1;
       this.activeTab = key === '1' ? 'doctor-info' : 'patient-info';
     },
   },
